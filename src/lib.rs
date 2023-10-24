00dcb002cc011e41f64d293537be8fbc7566f7be
BIGNARF <86953676+BIGNARF@users.noreply.github.com>
2023-10-24 20:14:54 -0300

    routine

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,43 @@
-use jammdb::{Data, Error, DB};
-use serde::{Deserialize, Serialize};
-/*
-pub struct WSModule{
-    assets :DB
-} 
-pub impl engine_core::Module for WSModule{
-    fn start(&self){    
-        &self.assets=DB::open("my-database.db")?;
-    }
+use sqlite::{ Statement, Connection};
 
-    fn finish(&self){
-
-    }
-}*/
-
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
-struct User {
-    username: String,
-    password: String,
+pub struct blob_holder{
+    connection: Connection
 }
 
-pub fn add_to_db() -> Result<(), Error> {
-    let user = User {
-        username: "my-user".to_string(),
-        password: "my-password".to_string(),
-    };
+impl blob_holder{
+    pub fn new()->blob_holder{
+        blob_holder { connection: sqlite::open("database.db").unwrap() }
+    }
 
-    // open a new database file and start a writable transaction
-    let db = DB::open("my-database.db")?;
-    let tx = db.tx(true)?;
-
-    // create a bucket to store users
-    let users_bucket = tx.create_bucket("users")?;
-
-    // serialize struct to bytes and store in bucket
-    let user_bytes = rmp_serde::to_vec(&user).unwrap();
-    users_bucket.put("user1", user_bytes)?;
-
-    // commit the changes so they are saved to disk
-    tx.commit()?;
-
-    Ok(())
-}
-
-pub fn read_from_db() -> Result<(), Error> {
-    // open the existing database file
-    let db = DB::open("my-database.db")?;
-
-    // open a read-only transaction to get the data
-    let tx = db.tx(false)?;
-
-    // get the bucket we created in the last transaction
-    let users_bucket = tx.get_bucket("users")?;
-
-    // get the key / value pair we inserted into the bucket
-    if let Some(data) = users_bucket.get("user1") {
-        if data.is_kv() {
-            let kv = data.kv();
-            // deserialize into a user struct
-            let db_user: User = rmp_serde::from_slice(kv.value()).unwrap();
-            println!("pass:{}, name:{}", db_user.password, db_user.username);
-            //assert_eq!(db_user, user);
+    pub fn build(&self, Table: &str)->sqlite::Result<()>{
+        let query =format!("CREATE TABLE {} (name TEXT,file BLOB);",Table);
+        self.connection.execute(query.as_str())?;
+        Ok(())
+    }
+    
+    pub fn add_blob(&self,table: &str,name: &str, data: &[u8])->sqlite::Result<()> {
+        let query = format!("INSERT INTO {} VALUES (:name,:data);", table);
+        let mut statement= self.connection.prepare(query.as_str())?;
+        statement.bind((":name",name));
+        statement.bind((":data",data));
+        while let Ok(sqlite::State::Row) = statement.next() {
+            statement.read::<String, _>("name").unwrap();
         }
+        Ok(())
     }
-    //Err(Error::BucketExists)
-    Ok(())
+    
+    pub fn read_blob(&self,Table: &str,Value: &str)->sqlite::Result<Vec<u8>> {
+        let query =format!("SELECT * FROM {} WHERE name = :name;",Table);
+    
+        let mut statement = self.connection.prepare(query.as_str()).unwrap();
+        statement.bind((":name",Value))?;
+        
+        let mut res :Vec<u8>=vec![];
+        while let Ok(sqlite::State::Row) = statement.next() {
+            res=statement.read::<Vec<u8>, _>("file").unwrap();
+            statement.read::<Vec<u8>, _>("file").unwrap();
+        }
+        Ok(res)
+    }
+     
 }
